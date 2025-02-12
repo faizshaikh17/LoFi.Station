c69c03125d970dfd99db8a94f23b9987ccc78342
faizshaikh17 <mohdfaiz8101@gmail.com>
2025-02-13 00:40:48 +0530

    fix: deployment

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,12 +4,11 @@ import { Github } from "lucide-react";
 
 export function Footer() {
     return (
-        <footer className="bg-background px-6 py-4 top-[41rem] w-full fixed bottom-0 left-0">
+        <footer className="bg-background px-6 py-4 h-[3rem] bg-amber-200 w-full fixed bottom-0 left-0">
             <div className="flex justify-end items-end gap-2">
                 <p className="text-right text-[#73e7e7] flex gap-2 text-sm">© Designed with <img src="src/assets/pixel-heart.png" alt="" width={20} height={20} /> by Faiz Shaikh.</p>
             </div>
         </footer>
     );
 }
-
 export default Footer;
