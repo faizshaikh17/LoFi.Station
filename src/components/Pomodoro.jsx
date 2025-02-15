7b4d4d514948df90f623e4ef636201a8093212c7
faizshaikh17 <mohdfaiz8101@gmail.com>
2025-02-16 03:20:33 +0530

    pomo reducers added

--- src/components/Pomodoro.jsx
+++ src/components/Pomodoro.jsx
@@ -1,15 +1,19 @@
 import React, { useState } from 'react'
 import { ChevronLeft, ChevronRight } from "lucide-react";
-
+import { useDispatch, useSelector } from 'react-redux';
+import { startTimer, pauseTimer, tick, reset } from '../feature/pomodoroSlice'
 function Pomodoro() {
-  const [timer, setTimer] = useState(17);
+  const { workDuration, breakDuration, timeLeft, isRunning, isWorkInterval } = useSelector((state) => state.pomodoro)
+  const dispatch = useDispatch();
+  const minutes = timeLeft / 60;
+  const seconds = minutes % 60
 
 
   return (
     <div className='flex bg-[#171717] z-[10] gap-2 border-[#73e7e7] text-[#e5e6e6] border-1 absolute -right-8 flex-col bg-primary w-50 items-center justify-center p-4 m-8 shadow-lg'>
       <div className='flex justify-around items-center gap-2'>
         <ChevronLeft />
-        <h1 className='text-4xl font-\ text-[#e5e6e6]'>05:00</h1>
+        <h1 className='text-4xl'>{`${minutes}:${seconds}`}</h1>
         <ChevronRight />
       </div>
       <div className='flex justify-between gap-4'>
@@ -17,13 +21,14 @@ function Pomodoro() {
           type="submit"
           // disabled={loading}
           className="hover:bg-[#e5e6e6] border-1 border-[#e5e6e6] font-bold hover:text-[#171717] text-sm w-17 bg-center hover:cursor-[url(/assets/cursors/pointer.png),_pointer] h-9 px-4 "
+          onClick={() => { dispatch(startTimer(), tick()) }}
         >
           {"Start"}
         </button>
         <button
           type="submit"
           // disabled={loading}
-          className="bg-red-500 font-bold text-[#171717] text-sm w-17 bg-center hover:cursor-[url(/assets/cursors/pointer.png),_pointer] h-9 px-4 "
+          className="bg-[#ff2400] font-bold text-[#171717] text-sm w-17 bg-center hover:cursor-[url(/assets/cursors/pointer.png),_pointer] h-9 px-4 "
         >
           {"Reset"}
         </button>
--- src/feature/pomodoroSlice.js
+++ src/feature/pomodoroSlice.js
@@ -1,7 +1,11 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialStatePomodoro = {
-    
+    workDuration: 25 * 60,
+    breakDuration: 5 * 60,
+    timeLeft: 25 * 60,
+    isRunning: false,
+    isWorkInterval: true
 };
 
 const pomodoroSlice = createSlice({
@@ -10,10 +14,27 @@ const pomodoroSlice = createSlice({
     reducers: {
         subTime: () => { },
         addTime: () => { },
-        startTimer: () => { },
-        reset: () => { }
+        startTimer: (state) => {
+            state.isRunning = true;
+        },
+        pauseTimer: () => {
+            state.isRunning = false;
+        },
+        tick: (state) => {
+            if (state.timeLeft > 0) {
+                state.timeLeft--
+            } else {
+                state.isWorkInterval = !state.isWorkInterval;
+                state.timeLeft = state.isWorkInterval ? state.workDuration : state.breakDuration
+            }
+        },
+        reset: (state) => {
+            state.isRunning = false;
+            state.timeLeft = state.timerDuration
+            state.isWorkInterval = false
+        }
     }
 })
 
-export const { subTime, addTime, startTimer, reset } = pomodoroSlice.actions;
+export const { startTimer, pauseTimer, tick, reset } = pomodoroSlice.actions;
 export default pomodoroSlice.reducer
