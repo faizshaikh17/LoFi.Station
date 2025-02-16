047f2f11f42e53364c018a5709b7802eca135c82
faizshaikh17 <mohdfaiz8101@gmail.com>
2025-02-16 18:58:12 +0530

    pomo ready - addTime & subTime

--- src/components/Pomodoro.jsx
+++ src/components/Pomodoro.jsx
@@ -1,23 +1,38 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import { ChevronLeft, ChevronRight } from "lucide-react";
 import { useDispatch, useSelector } from 'react-redux';
 import { startTimer, pauseTimer, tick, reset } from '../feature/pomodoroSlice'
 function Pomodoro() {
   const { workDuration, breakDuration, timeLeft, isRunning, isWorkInterval } = useSelector((state) => state.pomodoro)
+  const intervalRef = useRef(null)
+  console.log(isRunning, timeLeft);
 
-  console.log(isRunning);
+  const tickInterval = () => {
+    if (intervalRef.current) clearInterval(intervalRef.current)
+    intervalRef.current = setInterval(() => {
+      dispatch(tick())
+    }, 1000);
+  }
 
 
   const dispatch = useDispatch();
-  const minutes = Math.floor(timeLeft / 60)
-  const seconds = (timeLeft % 60)
 
   const handleStartTimer = () => {
-    dispatch(startTimer())
-    setInterval(() => {
-      dispatch(tick());
-    }, 1000)
+    if (!isRunning) {
+      dispatch(startTimer())
+      tickInterval();
+    }
+    else {
+      dispatch(pauseTimer());
+      clearInterval(intervalRef.current)
+
+    }
   }
+
+
+
+  const minutes = Math.floor(timeLeft / 60)
+  const seconds = (timeLeft % 60)
 
   return (
     <div className='flex bg-[#171717] z-[10] gap-2 border-[#73e7e7] text-[#e5e6e6] border-1 absolute -right-8 flex-col bg-primary w-50 items-center justify-center p-4 m-8 shadow-lg'>
@@ -29,16 +44,17 @@ function Pomodoro() {
       <div className='flex justify-between gap-4'>
         <button
           type="submit"
+          ref={intervalRef}
           // disabled={loading}
-          className="hover:bg-[#e5e6e6] border-1 border-[#e5e6e6] font-bold hover:text-[#171717] text-sm w-17 bg-center hover:cursor-[url(/assets/cursors/pointer.png),_pointer] h-9 px-4 "
+          className="hover:bg-[#e5e6e6] border-1 border-[#e5e6e6]  hover:text-[#171717] text-sm w-17 bg-center hover:cursor-[url(/assets/cursors/pointer.png),_pointer] h-9 px-4 "
           onClick={() => { handleStartTimer() }}
         >
-          {isRunning ? "Pause" : "Start"}
+          {!isRunning ? "Start" : "Pause"}
         </button>
         <button
           type="submit"
           // disabled={loading}
-          className="bg-[#ff2400] font-bold text-[#171717] text-sm w-17 bg-center hover:cursor-[url(/assets/cursors/pointer.png),_pointer] h-9 px-4 "
+          className="bg-[#ff4433] hover:bg-[#ff3131] text-sm w-17 bg-center hover:cursor-[url(/assets/cursors/pointer.png),_pointer] h-9 px-4 "
           onClick={() => { dispatch(reset()) }}
         >
           {"Reset"}
